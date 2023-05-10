614becf69991f3a981ecb3890ffbf8168325c480
mimototo <mail@mimototo.jp>
2023-05-10 21:33:36 +0900

    fix: 🐛 const
    add const file
    
    BREAKING CHANGE: 🧨 no
    
    ✅ Closes: no

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'craftzdog copy'
+export const SITE_DESCRIPTION = 'just copy demo test learn how to use astro'
+export const HOMEPAGE_URL = 'https:www.craftz.dog/'
